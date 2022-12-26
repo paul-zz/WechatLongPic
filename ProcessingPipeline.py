0b6a07b4519649c6431f176f06419c6e359df047
paul-zz <593958169@qq.com>
2022-12-27 00:56:52 +0100

    removed debug prints

--- ProcessingPipeline.py
+++ ProcessingPipeline.py
@@ -40,4 +40,3 @@ class ProcessingPipeline:
         pic_arranger.set_output_width(self.config_dict["output_width"])
         pic_arranger.generate_image()
         pic_arranger.save_output(self.config_dict["output"])
-        pic_arranger.output_img.show()
