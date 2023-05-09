0813536cc925b5e13367dc6a9b469371ceddc876
paul-zz <593958169@qq.com>
2023-05-09 23:38:46 +0800

    tempfile

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,2 @@
-        draw = ImageDraw.Draw(rendered_img)
-        caption_size = draw.textsize(self.caption_text, self.caption_font)
-        subtitle_size = draw.textsize(self.subtitle_text, self.subtitle_font)
-        caption_x = self.size // 2 - caption_size[0] // 2
-        caption_y = self.size // 2 - caption_size[1] - self.caption_offset
-        subtitle_x = self.size // 2 - subtitle_size[0] // 2
-        subtitle_y = self.size // 2 + self.caption_offset
-        draw.text((caption_x, caption_y), self.caption_text, self.caption_color, font=self.caption_font)
-        draw.text((subtitle_x, subtitle_y), self.subtitle_text, self.subtitle_color, font=self.subtitle_font)
+        # self.layout.addWidget(self.tabs)
+        # self.setLayout(self.layout)
