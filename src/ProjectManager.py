77fa54b655b877fc0258394af8376b0295ae873d
Zijian ZHONG <593958169@qq.com>
2023-06-26 00:06:02 +0800

    created declaration of the ProjectManager class

---
+++ src/ProjectManager.py
@@ -0,0 +1,14 @@
+import json
+
+class ProjectManager:
+    def __init__(self):
+        pass
+
+    def checkProjectFileValidity(self, filename : str):
+        pass
+
+    def parseProjectFile(self, filename : str):
+        pass
+
+    def writeProjectFile(self, filename : str):
+        pass
